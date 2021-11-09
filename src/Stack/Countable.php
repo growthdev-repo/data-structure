44541d76526601ec1ba3796555509b54ce54ce46
Walmir Silva <walmir.design@gmail.com>
2021-11-08 23:16:54 -0300

    create interfaces countable and iterator

---
+++ src/Stack/Countable.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrowthDev\DataStructure\Stack;
+
+interface Countable
+{
+    public function count(): int;
+}
---
+++ src/Stack/Itarator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrowthDev\DataStructure\Stack;
+
+interface Iterator
+{
+    public function hasNext(): bool;
+
+    public function next(): ?object;
+}
